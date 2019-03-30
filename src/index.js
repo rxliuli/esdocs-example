3cb260a7c0e9460768778f58009da0ee5bd58a45
rxliuli <rxliuli@gmail.com>
2019-03-30 23:25:10 +0800

    添加一些测试用的工具函数

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { add } from './add'
+import { uniqueBy } from './uniqueBy'
+import { wait } from './wait'
+
+const rx = {
+  add,
+  uniqueBy,
+  wait,
+}
+
+module.exports = rx
+export default rx
